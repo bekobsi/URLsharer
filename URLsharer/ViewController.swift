b39b372a6c471c94ac2541b37906e6cda3975cb5
bekobsi <naoya97316@gmail.com>
2021-06-16 00:58:50 +0900

    Initial Commit

---
+++ URLsharer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLsharer
+//
+//  Created by 原直也 on 2021/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
