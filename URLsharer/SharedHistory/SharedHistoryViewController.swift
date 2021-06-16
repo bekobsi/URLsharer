8e54f12e0c59c34e1dc7adb0d8ce97b3c0ec1508
bekobsi <naoya97316@gmail.com>
2021-06-16 20:42:21 +0900

    mainStoryboardをSharedHistoryに変更しました。

---
+++ URLsharer/SharedHistory/SharedHistoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SharedHistoryViewController.swift
+//  URLsharer
+//
+//  Created by 原直也 on 2021/06/16.
+//
+
+import UIKit
+
+class SharedHistoryViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
