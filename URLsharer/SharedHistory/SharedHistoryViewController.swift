518dd01f465fbd23dd3b7ce4d3c4863dd85e4fa5
bekobsi <naoya97316@gmail.com>
2021-06-18 21:14:55 +0900

    Share Extensionを追加しました

--- URLsharer/SharedHistory/SharedHistoryViewController.swift
+++ URLsharer/SharedHistory/SharedHistoryViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class SharedHistoryViewController: UIViewController {
+    @IBOutlet var testImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let QRimage = generateQRCode(from: "https://github.com/el-hoshino/QuickshaRe")
+
+        testImage.image = QRimage
+    }
+
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+        if let QRFilter = CIFilter(name: "CIQRCodeGenerator") {
+            QRFilter.setValue(data, forKey: "inputMessage")
+            guard let QRImage = QRFilter.outputImage else { return nil }
+            return UIImage(ciImage: QRImage)
+        }
+        return nil
     }
 }
