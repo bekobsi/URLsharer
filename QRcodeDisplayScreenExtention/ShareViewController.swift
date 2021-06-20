73e9cf6e5dc0e118973dc1d097b6c8f5c8265579
bekobsi <naoya97316@gmail.com>
2021-06-20 21:44:14 +0900

    現在開いているサイトのQRコードの生成出来るようにしました

--- QRcodeDisplayScreenExtention/ShareViewController.swift
+++ QRcodeDisplayScreenExtention/ShareViewController.swift
@@ -5,24 +5,45 @@
 //  Created by 原直也 on 2021/06/17.
 //
 
+import MobileCoreServices
 import Social
 import UIKit
 
-class ShareViewController: SLComposeServiceViewController {
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
+
+class ShareViewController: UIViewController {
+    @IBOutlet var testImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let extensionItem: NSExtensionItem = extensionContext?.inputItems.first as! NSExtensionItem
+        let itemProvider = extensionItem.attachments?.first as! NSItemProvider
+
+        let puclicURL = String(kUTTypeURL) // "public.url"
+        print("puclicURL:", puclicURL)
+        // shareExtension で NSURL を取得
+        if itemProvider.hasItemConformingToTypeIdentifier(puclicURL) {
+            itemProvider.loadItem(forTypeIdentifier: puclicURL, options: nil, completionHandler: { item, _ in
+                // NSURLを取得する
+                if let url: NSURL = item as? NSURL {
+                    // ----------
+                    // 保存処理
+                    // ----------
+                    print("url", url.absoluteString!)
+                    let QRimage = self.generateQRCode(from: url.absoluteString!)
+                    self.testImage.image = QRimage
+                }
+            })
+        }
+
+//        let QRimage = generateQRCode(from: "https://github.com/el-hoshino/QuickshaRe")
     }
 
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-    }
-
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+        if let QRFilter = CIFilter(name: "CIQRCodeGenerator") {
+            QRFilter.setValue(data, forKey: "inputMessage")
+            guard let QRImage = QRFilter.outputImage else { return nil }
+            return UIImage(ciImage: QRImage)
+        }
+        return nil
     }
 }
