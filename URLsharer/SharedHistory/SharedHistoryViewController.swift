110d20c6b3ef0248be6051544c469f147578ef38
bekobsi <naoya97316@gmail.com>
2021-11-14 23:29:22 +0900

    SharedHistoryView内で使われるCellのUIを作成しました。

--- URLsharer/SharedHistory/SharedHistoryViewController.swift
+++ URLsharer/SharedHistory/SharedHistoryViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class SharedHistoryViewController: UIViewController {
+    @IBOutlet var sharedHistoryTableView: UITableView!
+
+    let todo = ["渋谷", "五反田", "新宿"]
     private var presenter: SharedHistoryPresenterInput!
     func inject(presenter: SharedHistoryPresenterInput) {
         self.presenter = presenter
@@ -15,7 +18,24 @@ class SharedHistoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        sharedHistoryTableView.register(UINib(nibName: "URLHistoryTableVIewCell", bundle: nil), forCellReuseIdentifier: "CustomCell")
     }
 }
 
 extension SharedHistoryViewController: SharedHistoryPresenterOutput {}
+
+extension SharedHistoryViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return todo.count
+    }
+
+    func tableView(_: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = sharedHistoryTableView.dequeueReusableCell(withIdentifier: "CustomCell") as! URLHistoryTableVIewCell
+        cell.textLabel?.text = todo[indexPath.row]
+        return cell
+    }
+
+    func tableView(_: UITableView, heightForRowAt _: IndexPath) -> CGFloat {
+        return 100
+    }
+}
---
+++ URLsharer/Xibs/URLHistoryTableVIewCell.swift
@@ -0,0 +1,10 @@
+//
+//  URLHistoryTableVIewCell.swift
+//  URLsharer
+//
+//  Created by 原直也 on 2021/11/10.
+//
+
+import UIKit
+
+class URLHistoryTableVIewCell: UITableViewCell {}
